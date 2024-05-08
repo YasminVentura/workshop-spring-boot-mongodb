d9cbac2d4ba438ea4d66c221e106c92987e840f8
Yasmin Ventura <yasmin.gomsv@gmail.com>
2024-05-08 16:28:41 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/yasmingv/workshopmongo/resources/UserResource.java
+++ src/main/java/com/yasmingv/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.yasmingv.workshopmongo.resources;
 
+import com.yasmingv.workshopmongo.domain.Post;
 import com.yasmingv.workshopmongo.domain.User;
 import com.yasmingv.workshopmongo.dto.UserDTO;
 import com.yasmingv.workshopmongo.services.UserService;
@@ -52,5 +53,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
